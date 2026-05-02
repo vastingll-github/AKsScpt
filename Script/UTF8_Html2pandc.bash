95dac0fd813f3c69c93e012f58728d36554f0460
still.stingll <still.stingll@gmail.com>
2026-05-02 09:21:15 +0900

    mkascidoc Usage adds

--- Script/UTF8_Html2pandc.bash
+++ Script/UTF8_Html2pandc.bash
@@ -5,6 +5,11 @@
 # 4. bookmark で章(ShellScript)節(制御・変数等)項(htmlの題目) でディレクトリやasciidoc ファイルを作成する
 # ./UTF8_Html2pandc.bash ${URL} ${CURDIR}/${SETSDIR}
 
+function usage {
+	echo -e "\n\n   $0 URL StoredDir\n\n"
+	exit 1
+}
+
 #DTTM=`date +%Y%m%d_%H%M%S`
 CMDNM00=${0%\.*}
 CMDNM01=${CMDNM00#\./}
